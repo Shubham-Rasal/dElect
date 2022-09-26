77dfdd45907c0abf928850b8e3291be7a6fb636e
Shubham-Rasal <bluequbits@gmail.com>
2022-09-26 23:27:09 +0530

    improve ui

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,17 @@ const App = () => {
   const contractAddress = "0xF9963A9269C9330dd221ac1375Ee60280502Fb39"
   const contract = new ethers.Contract(contractAddress, contractABI, provider.getSigner())
 
-  if (accounts.length !== 0) {
-    setIsConnected(true)
-  }
-
-  async function getAdmin() {
 
 
-    const res = await contract.admins("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4")
-    
-    console.log(res.id.toString(),res.name,res.adminAddress.toString())
-    if(res.id == 0)
-    console.log("Not an admin")
+  async function checkVoter() {
+    const voter = await contract.voters("0xeb7e64ec981a178a70eba05a37b3ee5e440df041");
+    console.log(voter.name)
+
   }
 
   if (isConnected) {
 
-     getAdmin()
+    checkVoter()
 
 
 
@@ -41,12 +35,47 @@ const App = () => {
 
     return (
       <>
-        <h1 className="bg-amber-500">Connected as : {state.accounts[0]}</h1>
-        {
-          1 == 1 ?
-            <h1>HEllo</h1> : <Landing />
+        {/* <h1 className="bg-amber-500">Connected as : {state.accounts[0]}</h1> */}
+        <div className="flex flex-col w-screen h-screen  items-start  bg-slate-600 overflow-x-hidden">
+          <div className="flex w-full  justify-end bg-slate-500 overflow-x-hidden">
 
-        }
+            {/* <button className="p-3 pl-5 pr-5 m-2
+            bg-slate-900 text-amber-300 text-lg
+            font-semibold rounded-md tracking-wider
+            transform hover:shadow-lg transition duration-500
+            ">
+              VOTE
+            </button> */}
+            <button className="p-3 pl-5 pr-5 m-2
+            text-slate-900 bg-amber-300 text-lg
+            font-semibold rounded-md tracking-wider
+            transform hover:shadow-lg transition duration-500
+            hover:ring-slate-900 ring-2
+            ">
+              Create
+            </button>
+            <button className="p-3 pl-5 pr-5 m-2
+            text-slate-900 bg-amber-300 text-lg
+            font-semibold rounded-md tracking-wider
+            transform hover:shadow-lg transition duration-700
+            hover:ring-amber-200 ring
+            ">
+              Apply
+            </button>
+
+          </div>
+          <div className="flex flex-col w-75">
+            <h1>Active Elections to vote for</h1>
+            <div className="flex h-50 bg-slate-500">
+              <div className="w-screen bg-slate-200 h-9"> 
+              this is the description of the Election
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
       </>
 
     )
--- src/GlobalContext.jsx
+++ src/GlobalContext.jsx
@@ -2,8 +2,11 @@ import { useState, createContext } from 'react';
 
 export const GlobalContext = createContext();
 
+const { accounts } = window.ethereum._state;
+const c = (accounts.length !== 0)
 export const GlobalContextProvider = (props) => {
-    const [isConnected, setIsConnected] = useState(false);
+    const [isConnected, setIsConnected] = useState(c);
+
 
     return (
         <GlobalContext.Provider value={[isConnected, setIsConnected]}>
