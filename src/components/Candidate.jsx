04b3eeeb19afc42a5a478d0bfe58fcddf9f447bb
Shubham-Rasal <bluequbits@gmail.com>
2022-09-29 11:44:04 +0530

    candidate dashboard  Ui improved

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -1,30 +1,31 @@
 import React, { useState ,useEffect ,useContext } from 'react'
 import { GlobalContext } from '../GlobalContext'
+import Button from './Button'
+import { CandidateElection } from './Election'
 
 const Candidate = () => {
 
     const {contract} = useContext(GlobalContext)
 
     const [statusClass, setStatusClass] = useState({
-        className: 'bg-green-500',
+        className: 'bg-green-300',
         status: 'active'
     })
-    const [elections,setElctions] = useState([]);
+    const [elections,setElections] = useState([]);
     useEffect(() => {
-        const elections = [];
+        let elections = [];
 
         (async()=>{
             const electionCount = await contract.electionCount();
-            console.log(Number(electionCount.toString()))
+            console.log("NO of Elections : ",Number(electionCount.toString()))
 
             for(let i=1;i<=electionCount;i++){
 
-                const election = await  contract.elections(i);
-                console.log(election)
+                const election = await  contract.elections(i);                
                 elections.push(election)
 
             }
-            console.log(elections)
+            setElections(elections)
 
         })()
 
@@ -42,16 +43,42 @@ const Candidate = () => {
         <div className="flex flex-col">
             <div className="active-elections w-screen">
                 <div className="bg-red-300 "> Active Elections</div>
+                {elections.map((election,index) =>{
+                    // console.log(election)
+                    return(
+                        <CandidateElection key={index} election={election}/>
+                    )
+                })}
 
             </div>
             <div className="standing flex flex-col items-center">
-                <div className="bg-red-500 w-screen"> Approved Elections: </div>
-                <div className="election-card ml-5 mt-3 w-2/4 bg-blue-200">
-                    <div className={`name flex items-center p-1  font-semibold text-yellow-900`}>Mid-Term Elections
-                        <span className={`status ${statusClass.className}   text-white p-1  m-1 rounded-lg`}>{statusClass.status}</span>
+                <div className="0 w-screen"> Approved Elections: </div>
+                <div className="election-card  ml-5 mt-3 w-2/4 shadow-xl rounded-md bg-slate-100 hover:scale-105 transition-all duration-200 ease-in">
+                    <div className={`name flex justify-between  items-center p-1 border-green-400 border-y-2 px-2 font-semibold text-lg  `}>Mid-Term Elections
+                        <span className={`status ${statusClass.className}    p-1 px-2   m-1 rounded-full`}>{statusClass.status}</span>
                     </div>
-                    <div className={`results-live text-lg p-2 bg-blue-300 text-blue-900`}>Votes: 20</div>
-                    <div className={`results-live text-lg p-2 bg-blue-300 text-blue-900`}>Lost</div>
+                    <div className={`flex justify-between font-bold text-slate-500 results-live text-xl p-2 `}>Votes:
+                    <div>20</div>
+                    </div>
+                    <div className={`results-live text-xl font-semibold p-2 `}><span className='text-slate-500'>Status :</span>  Lost</div>
+                </div>
+                <div className="election-card  ml-5 mt-3 w-2/4 shadow-xl">
+                    <div className={`name flex items-center p-1 px-2 font-semibold `}>Mid-Term Elections
+                        <span className={`status ${statusClass.className}   p-1  m-1 rounded-lg`}>{statusClass.status}</span>
+                    </div>
+                    <div className={`flex justify-between font-bold text-slate-500 results-live text-xl p-2 `}>Votes:
+                    <div>20</div>
+                    </div>
+                    <div className={`results-live text-xl font-semibold p-2 `}><span className='text-slate-500'>Status :</span>  Lost</div>
+                </div>
+                <div className="election-card  ml-5 mt-3 w-2/4 shadow-xl">
+                    <div className={`name flex items-center p-1 px-2 font-semibold `}>Mid-Term Elections
+                        <span className={`status ${statusClass.className}   p-1  m-1 rounded-lg`}>{statusClass.status}</span>
+                    </div>
+                    <div className={`flex justify-between font-bold text-slate-500 results-live text-xl p-2 `}>Votes:
+                    <div>20</div>
+                    </div>
+                    <div className={`results-live text-xl font-semibold p-2 `}><span className='text-slate-500'>Status :</span>  Lost</div>
                 </div>
             </div>
         </div>
--- src/components/Election.jsx
+++ src/components/Election.jsx
@@ -1,34 +1,49 @@
 import React from 'react'
+import Button from './Button'
 
 const Election = () => {
   return (
     <div className="flex flex-col sm:w-2/4 w-auto bg-slate-500 m-3 p-3 rounded-md ">
-              <div className="title text-lg font-bold bg-slate-100 m-2 p-2 rounded-md shadow-xl">MID-TERM GENERAL ELECTION</div>
-              <div className=" bg-slate-200 m-2 p-2 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Velit delectus sed temporibus asperiores molestiae nihil ex
-                iste debitis voluptas architecto. Quas, culpa dicta tempora
-                possimus expedita ipsam. Maxime, sapiente illo!
-              </div>
-              <div className="criteria p-2">
-                <i><b>Criteria</b></i>
-                <li className="hover:bg-amber-300 text-xl font-semibold">Should be matic token holder</li>
-                <li className="hover:bg-amber-400 text-xl font-semibold">Should be matic token holder</li>
-                <li className="hover:bg-amber-500 text-xl font-semibold">Should be matic token holder</li>
+      <div className="title text-lg font-bold bg-slate-100 m-2 p-2 rounded-md shadow-xl">MID-TERM GENERAL ELECTION</div>
+      <div className=" bg-slate-200 m-2 p-2 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Velit delectus sed temporibus asperiores molestiae nihil ex
+        iste debitis voluptas architecto. Quas, culpa dicta tempora
+        possimus expedita ipsam. Maxime, sapiente illo!
+      </div>
+      <div className="criteria p-2">
+        <i><b>Criteria</b></i>
+        <li className="hover:bg-amber-300 text-xl font-semibold">Should be matic token holder</li>
+        <li className="hover:bg-amber-400 text-xl font-semibold">Should be matic token holder</li>
+        <li className="hover:bg-amber-500 text-xl font-semibold">Should be matic token holder</li>
 
-              </div>
-              <div className="vote">
-                <button className="p-3 pl-5 pr-5 m-2
+      </div>
+      <div className="vote">
+        <button className="p-3 pl-5 pr-5 m-2
             bg-slate-900 text-amber-300 text-lg
             font-semibold rounded-md tracking-wider
             transform hover:shadow-lg transition duration-500
             ">
-                  VOTE
-                </button>
-              </div>
+          VOTE
+        </button>
+      </div>
 
-            </div>
+    </div>
   )
 }
+
+export const CandidateElection = ({ election }) => {
+  return (
+    <div className="flex flex-col border-spacing-2 border-green-500 border-2">
+      <div className="name">name : {election.name}</div>
+      <div className="admin">Admin : {election.admin}</div>
+      <div className="start">Starts : {election.startTime.toString()}</div>
+      <div className="time left">Time left to apply: 40days</div>
+      <div className="apply"><Button name="Apply" /></div>
+    </div>
+  )
+}
+
+
 
 export default Election
