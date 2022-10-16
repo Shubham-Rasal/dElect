f0f245cb307fb45b6888d6b7d1d62de1e262eb74
Shubham-Rasal <bluequbits@gmail.com>
2022-10-16 10:28:14 +0530

    test

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
     return (
-        <button className=" bg-green-400 p-2 m-1 rounded-md px-3 text-lg ">
+        <button className=" bg-green-300 p-2 m-1 rounded-md px-3 text-lg ">
             {props.name}
         </button>
     )
